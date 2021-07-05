8d72acd4e9b5a5a03ac0e9482258bc19ff09b033
changyun <cyun9601@gmail.com>
2021-07-05 15:57:28 +0900

    kfold 제거

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ data.fillna(data.mean(), inplace=True)
 # Count the number of NaN values in each column
 print(data.isnull().sum())
 
+# %%
 # Split dataset into inputs and outputs 
 values = data.values
 X = values[:, 0:8]
@@ -23,22 +24,10 @@ Y = values[:, 8]
 # %% Data Load and Preprocessing
 
 # initiate the LR model with random hyperparameters 
-lr = LogisticRegression(penalty='l2', dual=False, max_iter=110)
+lr = LogisticRegression(penalty='l2', dual=False, max_iter=200)
 lr.fit(X, Y)
 
 print("lr score : ", lr.score(X, Y))
-
-# %% KFold
-
-# You will need the following dependencies for applying Cross-Validation and evaluating the cross-validated score
-from sklearn.model_selection import KFold, cross_val_score
-
-# Build the k-fold cross-validator
-kfold = KFold(n_splits=3, random_state=7, shuffle=True)
-
-result = cross_val_score(lr, X, Y, cv=kfold, scoring='accuracy')
-print(result.mean())
-
 
 # %% Gridsearch 
 from sklearn.model_selection import GridSearchCV
@@ -70,3 +59,5 @@ random_result = random.fit(X, Y)
 # Summarize results 
 print("Best: %f using %s" %(random_result.best_score_, random_result.best_params_))
 print("Execution time: " + str((time.time() - start_time)) + ' ms')
+
+# %%
